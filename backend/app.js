975ade21bca4bafb6583ce2a670797454086349d
Alina Rashitova <alinarashitova02@yandex.ru>
2023-02-12 14:59:01 +0400

    fixed_2

--- backend/app.js
+++ backend/app.js
@@ -54,8 +54,6 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  console.log(origin);
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
